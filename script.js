ab0dfa9853ed5067ff7ee45fe0121e2264c9e9b3
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-14 20:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-// complete the given function
+function palindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 
-function palindrome(str){
+  // Create reversed version of the string
+  const reversedStr = cleanedStr.split("").reverse().join("");
 
+  // Compare the original and reversed strings
+  return cleanedStr === reversedStr;
 }
-module.exports = palindrome
+
+module.exports = palindrome;
